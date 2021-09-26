fd0dee7faee5089f5b73c2ea1aacc43e5718394a
Ultima (≧ ∇ ≦) <81684610+ultimareall@users.noreply.github.com>
2021-09-26 14:32:04 +0700

    Update click.js

--- bot/js/click.js
+++ bot/js/click.js
@@ -1,3 +1,3 @@
 function openLink() {
-    window.open('https://api.whatsapp.com/send?phone=6288286421519&text=#help', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=62812788952868&text=#help', '_blank')
 }
